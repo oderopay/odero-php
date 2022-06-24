9182e0f4c4abb9190987034d980d1c44bfd0c4fc
Çağrı S <selam@bencagri.com>
2022-06-24 10:57:11 +0300

    Address Model specs

--- spec/Model/Address/BillingAddressSpec.php
+++ spec/Model/Address/BillingAddressSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Oderopay\Model\Address;
 
+use Oderopay\Model\Address\Address;
 use Oderopay\Model\Address\BillingAddress;
 use PhpSpec\ObjectBehavior;
 
@@ -10,5 +11,20 @@ class BillingAddressSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(BillingAddress::class);
+        $this->shouldHaveType(Address::class);
+    }
+
+    public function it_should_create_address()
+    {
+
+        $this
+            ->setAddress('address line')
+            ->setCity('istanbul')
+            ->setCountry('TUR')
+        ;
+
+        $this->getAddress()->shouldReturn('address line');
+        $this->getCity()->shouldReturn('istanbul');
+        $this->getCountry()->shouldReturn('TUR');
     }
 }
--- spec/Model/Address/DeliveryAddressSpec.php
+++ spec/Model/Address/DeliveryAddressSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Oderopay\Model\Address;
 
+use Oderopay\Model\Address\Address;
 use Oderopay\Model\Address\DeliveryAddress;
 use PhpSpec\ObjectBehavior;
 
@@ -10,5 +11,22 @@ class DeliveryAddressSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(DeliveryAddress::class);
+        $this->shouldHaveType(Address::class);
+    }
+
+    public function it_should_create_address()
+    {
+
+        $this
+            ->setAddress('address line')
+            ->setCity('istanbul')
+            ->setCountry('TUR')
+            ->setDeliveryType('Courier')
+        ;
+
+        $this->getAddress()->shouldReturn('address line');
+        $this->getCity()->shouldReturn('istanbul');
+        $this->getCountry()->shouldReturn('TUR');
+        $this->getDeliveryType()->shouldReturn('Courier');
     }
 }
