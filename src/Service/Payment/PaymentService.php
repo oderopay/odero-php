d3b5d5cd2ca0e604fe37844a57a2d7e656407c22
Çağrı S <selam@bencagri.com>
2022-06-28 14:01:26 +0300

    payment create fix for single payment

--- src/Service/Payment/PaymentService.php
+++ src/Service/Payment/PaymentService.php
@@ -4,6 +4,7 @@ namespace Oderopay\Service\Payment;
 
 use Oderopay\Http\Response\PaymentIntentResponse;
 use Oderopay\Http\Response\PaymentResponse;
+use Oderopay\Model\Payment\BasketItem;
 use Oderopay\Model\Payment\Merchant;
 use Oderopay\Model\Payment\Payment;
 use Oderopay\Service\BaseService;
@@ -18,13 +19,24 @@ class PaymentService extends BaseService
     {
         $payment->setMerchantId($this->client->config->getMerchantId());
 
+        $total = array_reduce($payment->getProducts(), function($sum, $product){
+            /** @var BasketItem $product */
+            $sum += $product->getTotal();
+            return $sum;
+        });
+
         if(empty($payment->getSubMerchants())){
             $merchant = new Merchant();
-            $merchant->setAmount(1); // total amount of products
+            $merchant->setAmount($total);
             $merchant->setName($this->client->config->getName());
             $merchant->setExtId($this->client->config->getMerchantId());
             $merchant->setCommission(0);
             $merchant->setProducts($payment->getProducts());
+            $payment->setSubMerchants([$merchant]);
+        }
+
+        if(!$payment->getAmount()){
+            $payment->setAmount($total);
         }
 
         $response = $this->request('POST','/api/payments/one-time', ['form_params' => $payment->toArray()]);
