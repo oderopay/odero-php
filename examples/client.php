ba48576e4da63d61a481a204c630f91e76b0bb46
Çağrı S <selam@bencagri.com>
2022-07-01 12:24:44 +0300

    change merchantId and token

--- examples/client.php
+++ examples/client.php
@@ -1,8 +1,8 @@
 <?php
 require_once "vendor/autoload.php";
 
-$merchantId = '0e20885b-611a-4601-c648-08da5813b2f9';
-$token = '5358f45a5d95bbf4eb669ed40e08c0ad';
+$merchantId = '{merchant-id}'; //uuid
+$token = '{merchant-token}';
 //Authentication is already handled by SDK
 $config = new \Oderopay\OderoConfig('My Store Name', $merchantId, $token, \Oderopay\OderoConfig::ENV_STG);
 
